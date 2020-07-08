18c74de638037b3a8ab5cfcb05acb8f97af91776
svetlana <s.sadkovskaia@campus-igs-toulouse.fr>
2020-07-08 14:15:22 +0200

    first commit

---
+++ src/main/java/org/example/webjava/controller/SimpleServlet.java
@@ -0,0 +1,25 @@
+package org.example.webjava.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SimpleServlet extends HttpServlet {
+
+    private  static  final  String TEST_MESSAGE = "SIMPLE SERVLET OK";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + TEST_MESSAGE + "</h1>");
+
+
+    }
+
+
+}
